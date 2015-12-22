5d18996514a4717ea8c9f6772430d6ec3207067a
Michael Bustamante <michaelbustamante@Michael-Bustamantes-MacBook-Pro.local>
2015-12-22 11:24:34 -0600

    fixed rusume page

--- resources/assets/js/components/Resume.vue
+++ resources/assets/js/components/Resume.vue
@@ -1,133 +1,109 @@
 <template>
-<h2>OBJECTIVE</h2>
+<div class="container">
+	<h2>OBJECTIVE</h2>
 
-<p>Seeking a challenging and rewarding position as a Web Developer, which will allow me to 
+	<p>Seeking a challenging and rewarding position as a Web Developer, which will allow me to exercise and expand my skills, while contributing to the success of an organization.</p>
 
-exercise and expand my skills, while contributing to the success of an organization.</p>
+	<h2>SUMMARY OF SKILLS</h2>
 
-<h2>SUMMARY OF SKILLS</h2>
+	<p>I obtain solid experiences in the IT field that allowed me to utilize my technical and problem-solving skills. I possess an ability that allows me to pay attention to detail, work productively with minimal supervision, and communicate technical concept to a non-technical audiences. I learn new technologies quickly and excel in all areas of the IT industry.</p>  
 
-<p>I obtain solid experiences in the IT field that allowed me to utilize my technical and problem-
+	<h2>AREAS OF EXPERIENCE</h2>
 
-solving skills. I possess an ability that allows me to pay attention to detail, 
+	<ul>
+		<li>HTML</li>
+		<li>CSS</li>
+		<li>PHP</li>
+		<li>JavaScript</li>
+		<li>MySql</li>
+		<li>Laravel</li>
+		<li>JQuery</li>
+		<li>Vue.js</li>
+		<li>Drupal</li>
+		<li>Wordpress</li>
+		<li>Joomla</li>
+		<li>Adobe</li>
+		<li>Photoshop</li>
+		<li>Illustrator</li>
+		<li>FileMaker</li>
+	</ul>
 
-work productively with minimal supervision, and communicate technical concept to 
 
-a non-technical audiences. I learn new technologies quickly and excel in all areas 
+	<h2>CERTIFICATIONS</h2>
+	<ul>
+		<li>CompTIA Network+</li>
+		<li>CompTIA A+</li>
+		<li>MCP</li>
+	</ul>
 
-of the IT industry.</p>  
+	<h2>EDUCATION</h2>
 
-<h2>AREAS OF EXPERIENCE</h2>
+	<p>2005-2006 Career Point Institute, Network Technician Program</p>
 
-<p>HTML, XHTML, HTML5, CSS PHP4, PHP5 
+	<h2>EMPLOYMENT</h2>
 
-Web Programming Languages 
+	<h3>Carnegie Technologies San Antonio TX, Sept 2014 - July 2015</h3>
 
-JavaScript/JQuery Zend, Laravel 
+	<h4>PHP Developer</h4> 
+	<ul>
+		<li>Develop student management system for private and charter schools</li>
+		<li>Utilize the Laravel framework and Twitter Bootstrap</li>
+		<li>Image manipulation, slicing and optimization using Photoshop</li>
+		<li>Browser compatibility testing with all major browser</li>
+		<li>Use Git version control and Redmine to track bugs</li>
+	</ul>
 
-Drupal Development Drupal, Joomla 
+	<h3>SPFM LP San Antonio TX, March 2013 - Sept 2014</h3>
+	<h4>Drupal Developer</h4> 
+	<ul>
+		<li>Provide front-end development & site building utilizing Drupal cms</li>
+		<li>Custom module building and Drupal Themeing</li>
+		<li>Image manipulation, slicing and optimization using Photoshop and Illustrator</li>
+		<li>Browser compatibility testing with all major browser</li>
+		<li>Create and send out HTML newsletters</li>
+	</ul>
 
-Adobe Photoshop CS5/6 Mysql, FileMaker/9/10/Server</p> 
+	<h3>Boss Creative San Antonio TX, July 2012 - Feb 2013</h3>
+	<h4>Drupal Developer</h4>
+	<ul>
+		<li>Provide front-end development & site building utilizing Drupal cms</li>
+		<li>Image manipulation, slicing and optimization using Photoshop</li>
+		<li>Browser compatibility testing with all major browser</li>
+		<li>Updated articles for Nside online magazine</li>
+	</ul>
 
-<h2>CERTIFICATIONS</h2>
-<ul>
-<li>CompTIA Network+</li> <li>MCP</li>
-</ul>
-<h2>EDUCATION</h2>
+	<h3>Texas Youth Hunting Program, San Antonio TX, August 2008 – July 2012</h3>
+	<h4>Webmaster/Database Admin/IT Support</h4>
+		<li>Developed Online Application System with FileMaker Backend </li>
+		<li>Utilized an expert knowledge of JavaScript, HTML, DOM, and CSS to deliver advanced user interfaces and controls </li>
+		<li>Computer hardware and software support MS OS</li>
+		<li>Design and Developed database with FileMaker Pro</li>
 
-<p>2005-2006 Career Point Institute, Network Technician Program</p>
+	<h2>Pre Web Experience</h2>
 
-<h2>EMPLOYMENT</h2>
+	<h3>2Wire, San Antonio TX August 2007- July 2008</h3>
+	<h4>Tier 1 Tech support</h4>
+	<ul>
+		<li>Phone/email support for TV, internet and VOIP issues in a call center environment</li>
+		<li>Walk customer through connecting wireless, setting up TV, and troubleshoot VOIP </li>
+		<li>Would also provide floor support to agents on the floor and mentor new agents</li>
+		<li>Document and record issues within the CRM database</li>
+	</ul>
+	<h2>REFERENCES</h2>
 
-<h3>Carnegie Technologies San Antonio TX, Sept 2014 - July 2015</h3>
+	<div>Rob Kinkaid - Supervisor @ SPFM LP - 210-763-2116</div>
+	<div>Mike Pilkiton - HR Manger @ Boss Creative - 210-390-0051</div>
+	<div>Quita Hill - Office Manger @ Texas Wildlife Association - 210-344-2240</div>
 
-<h4>PHP Developer</h4> 
-<ul>
-<li>Develop student management system for private and charter schools</li>
+	<h2>LINKS TO WORK</h2>
 
-<li>Utilize the Laravel framework and Twitter Bootstrap</li>
-
-<li>Image manipulation, slicing and optimization using Photoshop</li>
-
-<li>Browser compatibility testing with all major browser</li>
-
-<li>Use Git version control and Redmine to track bugs</li>
-</ul>
-<h3>SPFM LP San Antonio TX, March 2013 - Sept 2014</h3>
-
-<h4>Drupal Developer</h4> 
-<ul>
-<li>Provide front-end development & site building utilizing Drupal cms</li>
-
-<li>Custom module building and Drupal Themeing</li>
-
-<li>Image manipulation, slicing and optimization using Photoshop and Illustrator</li>
-
-<li>Browser compatibility testing with all major browser</li>
-
-<li>Create and send out HTML newsletters</li>
-</ul>
-Boss Creative San Antonio TX, July 2012 - Feb 2013
-
-Drupal Developer 
-
- Provide front-end development & site building utilizing Drupal cms
-
- Image manipulation, slicing and optimization using Photoshop
-
- Browser compatibility testing with all major browser
-
- Updated articles for Nside online magazine
-
-Texas Youth Hunting Program, San Antonio TX, August 2008 – July 2012
-
-Webmaster/Database Admin/IT Support
-
- Developed Online Application System with FileMaker Backend 
-
- Utilized an expert knowledge of JavaScript, HTML, DOM, and CSS to deliver advanced user 
-
-interfaces and controls 
-
- Computer hardware and software support MS OS
-
- Design and Developed database with FileMaker Pro
-
-Pre Web Experience 
-
-2Wire, San Antonio TX August 2007- July 2008
-
-Tier 1 Tech support
-
- Phone/email support for TV, internet and VOIP issues in a call center environment
-
- Walk customer through connecting wireless, setting up TV, and troubleshoot VOIP 
-
- Would also provide floor support to agents on the floor and mentor new agents
-
- Document and record issues within the CRM database
-
-REFERENCES
-
-Rob Kinkaid - Supervisor @ SPFM LP - 210-763-2116
-
-Mike Pilkiton - HR Manger @ Boss Creative - 210-390-0051
-
-Quita Hill - Office Manger @ Texas Wildlife Association - 210-344-2240
-
-LINKS TO WORK
-
-Schenley Learning - www.schenleylearning.com
-
-Livingston Lures - www.livingstonlures.com
-
-Ritter Dental - www.ritterdentalusa.com, ritter.dev.spfmlp.com
-
-Herman Sons Fraternal Insurance - www.texashermannsons.org
-
-San Antonio Family Law Section - www.safamilylaw.org
-
-Texas Leather Interiors - www.texasleatherinteriors.com
+	<div>Schenley Learning - www.schenleylearning.com</div>
+	<div>Livingston Lures - www.livingstonlures.com</div>
+	<div>Ritter Dental - www.ritterdentalusa.com, ritter.dev.spfmlp.com</div>
+	<div>Herman Sons Fraternal Insurance - www.texashermannsons.org</div>
+	<div>San Antonio Family Law Section - www.safamilylaw.org</div>
+	<div>Texas Leather Interiors - www.texasleatherinteriors.com</div>
+</div>
 </template>
 <script>
 	export default {
